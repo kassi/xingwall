94eb7770b502f2e7893b6444be7a2b6f6f016726
Blake Simpson <blake.simpson@xing.com>
2014-11-18 18:06:46 +0100

    Remove console log

--- app/controllers/profiles.js
+++ app/controllers/profiles.js
@@ -3,8 +3,6 @@ var Wall = require('mongoose').model('Wall');
 module.exports = function (app, io) {
   io.on('connection', function (socket) {
     socket.on('profiles:all', function (opts) {
-      console.log(opts);
-
       Wall.findOne({_id: opts.wallId}).exec()
         .then(function (wall) {
           socket.emit('profiles:all', wall.profiles);
