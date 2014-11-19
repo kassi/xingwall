4888fd57a7d438cc63cf152b7cacf05912c9cabe
Blake Simpson <blake.simpson@xing.com>
2014-11-19 10:39:43 +0100

    Turn QR code green

--- public/js/qr.js
+++ public/js/qr.js
@@ -1,8 +1,8 @@
 var qrcode = new QRCode(document.getElementById("qr"), {
-    text: window.location.href + '/connect',
+    text: window.location.href.replace('#', '') + '/connect',
     width: 128,
     height: 128,
-    colorDark : "#000000",
+    colorDark : "#006567",
     colorLight : "#ffffff",
     correctLevel : QRCode.CorrectLevel.L
 });
