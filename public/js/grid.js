f8a1f8eb004574e6402e74c3a081b31816e7165e
Blake Simpson <blake.simpson@xing.com>
2014-11-19 17:44:23 +0100

    Working version of the redesigned grid

--- public/js/grid.js
+++ public/js/grid.js
@@ -2,17 +2,21 @@ $(function() {
   var GRID_SIZE = 130; // 128px img + 2px of borders
 
   var $window   = $(window),
-    $wall       = $('.wall-profiles'),
-    $detail     = $('.wall-details'),
-    winWidth    = $window.outerWidth(),
-    winHeight   = $window.outerHeight(),
-    wallWidth   = $wall.outerWidth(),
-    detailWidth = $detail.outerWidth();
+    $wall       = $('.wall');
 
-  var columns        = Math.floor((winWidth - detailWidth) / GRID_SIZE);
-  var newWallWidth   = columns * GRID_SIZE;
-  var newDetailWidth = winWidth - newWallWidth;
+  function resizeWall () {
+    var winWidth     = $window.outerWidth();
+    var winHeight    = $window.outerHeight();
+    var wallWidth    = $wall.outerWidth();
+    var columns      = Math.floor(wallWidth / GRID_SIZE);
+    var newWallWidth = columns * GRID_SIZE;
+    var newOffset    = (winWidth - newWallWidth) / 2;
 
-  $wall.css('width', newWallWidth);
-  $detail.css('width', newDetailWidth);
+    $wall.css('left', newOffset);
+    $wall.css('right', newOffset);
+    $wall.css('top', newOffset);
+    $wall.css('bottom', newOffset);
+  }
+
+  resizeWall();
 });
