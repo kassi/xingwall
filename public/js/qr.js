4d70db2b34056059e530db0ca2c95fc74ba01256
Blake Simpson <blake.simpson@xing.com>
2014-11-19 10:22:20 +0100

    Remove actions bar and add a QR code

---
+++ public/js/qr.js
@@ -0,0 +1,8 @@
+var qrcode = new QRCode(document.getElementById("qr"), {
+    text: window.location.href + '/connect',
+    width: 128,
+    height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
