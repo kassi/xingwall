3191b4940f70a38c4fde90254cbf9fc4d7fa9909
Blake Simpson <blake.simpson@xing.com>
2014-11-20 12:28:44 +0100

    Add swipe

--- public/js/keys.js
+++ public/js/keys.js
@@ -3,6 +3,14 @@ $(function() {
     $detail   = $('.wall-details'),
     $close    = $detail.find('.close');
 
+  function showPrev () {
+    $('.wall-profile-entry.active').parent().prev().find('a').trigger('click');
+  }
+
+  function showNext () {
+    $('.wall-profile-entry.active').parent().next().find('a').trigger('click');
+  }
+
   $window.on('keyup', function(event) {
     // We only want keyboard actions if detail view is open
     if (!$detail.hasClass('active')) {
@@ -12,9 +20,21 @@ $(function() {
     if (event.keyCode === 27) { // Esc key to exit
       $close.trigger('click');
     } else if (event.keyCode === 37) { // Left arrow
-      $('.wall-profile-entry.active').parent().prev().find('a').trigger('click')
+      showPrev();
     } else if (event.keyCode === 39) { // Right arrow
-      $('.wall-profile-entry.active').parent().next().find('a').trigger('click')
+      showNext();
+    }
+  });
+
+  $window.swipe(function(direction, offset) {
+    if (offset.x < -200) {
+      showPrev();
+      console.log('prev');
+      return false;
+    } else if (offset.x > 200) {
+      showNext();
+      console.log('next');
+      return false;
     }
   });
 });
---
+++ public/js/swipe.js
@@ -0,0 +1,53 @@
+$(function() {
+  $.fn.swipe = function( callback ) {
+    var touchDown = false,
+      originalPosition = null,
+      $el = $( this );
+
+    function swipeInfo( event ) {
+      var x = event.originalEvent.pageX,
+        y = event.originalEvent.pageY,
+        dx, dy;
+
+      dx = ( x > originalPosition.x ) ? "right" : "left";
+      dy = ( y > originalPosition.y ) ? "down" : "up";
+
+      return {
+        direction: {
+          x: dx,
+          y: dy
+        },
+        offset: {
+          x: x - originalPosition.x,
+          y: originalPosition.y - y
+        }
+      };
+    }
+
+    $el.on( "touchstart mousedown", function ( event ) {
+      touchDown = true;
+      originalPosition = {
+        x: event.originalEvent.pageX,
+        y: event.originalEvent.pageY
+      };
+    } );
+
+    $el.on( "touchend mouseup", function () {
+      touchDown = false;
+      originalPosition = null;
+    } );
+
+    $el.on( "touchmove mousemove", function ( event ) {
+      if ( !touchDown ) { return;}
+      var info = swipeInfo( event );
+      var result = callback( info.direction, info.offset );
+
+      if (result === false) {
+        $el.trigger('touchend');
+        return result;
+      }
+    } );
+
+    return true;
+  };
+});
